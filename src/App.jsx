32e7de62615c8482e744003f97c4a625d38c69da
Damian Pereyra <pereyra.damian89@gmail.com>
2023-07-18 16:58:46 -0300

    BBVAFSW3T3-56- Creo pantalla inicio de sesion, valid y route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
-
-
-
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './components/auth/LoginPage';
+import Page from "./pages/index.jsx";
 
 function App() {
   return (
-    <>    
-    </>
+     <Page>
+       <Routes>
+        <Route path="/" element={<LoginPage/>}/>  
+        </Routes>
+        </Page>
   );
 }
-
 export default App;
