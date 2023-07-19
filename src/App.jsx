fa4e85783ee43e89dce4789210e13515b0e9544c
DiegoApro <diegoaprosoff@gmail.com>
2023-07-19 13:47:18 -0300

    BBVAFSW3T3-59

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
-
+import Tranferencia from './components/tranferencia/Tranferencia'
 
 
 
 function App() {
   return (
-    <>    
+    <>   
+    <Tranferencia/> 
     </>
   );
 }
