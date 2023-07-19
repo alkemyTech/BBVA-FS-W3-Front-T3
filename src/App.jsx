b928b69260f4aca9aabc6b1dbdff3d19855621cd
Damian Pereyra <pereyra.damian89@gmail.com>
2023-07-19 14:21:22 -0300

    BBVAFSW3T3-80- Creo RegisterPage/Elimino App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import "./App.css";
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './components/auth/LoginPage';
 import Page from "./pages/index.jsx";
+import RegisterPage from "./Components/auth/RegisterPage";
 
 function App() {
   return (
-     <Page>
-       <Routes>
-        <Route path="/" element={<LoginPage/>}/>  
-        </Routes>
-        </Page>
+    <Page>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Page>
   );
 }
 export default App;
