661865346c62b81041a1875060eb576db2533047
DiegoApro <diegoaprosoff@gmail.com>
2023-07-19 11:30:33 -0300

    Interfaz de usuario de “Realiza depósitos en su cuenta”

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Deposit from './components/deposit/deposit'
 
 
 
@@ -6,6 +7,7 @@ import "./App.css";
 function App() {
   return (
     <>    
+    <Deposit/>
     </>
   );
 }
