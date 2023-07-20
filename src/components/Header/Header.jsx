0b1b28cd4c5afb7d9e5cfc83bcdb0b97f7a5898d
robyspr <robysprenger@gmail.com>
2023-07-19 21:49:57 -0300

    Get dek user para el header en Page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
-import { React, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import {
   AppBar,
@@ -14,9 +15,53 @@ import { Menu, MenuItem } from "@mui/material";
 import { Grid, Tabs, Tab } from "@mui/material";
 
 import logo from "../../assets/3.png";
-import CustomAvatar from "../Icons/CustomAvatar";
+
+function stringToColor(string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name) {
+  if (!name || typeof name !== "string") {
+    // If the name is empty, null, undefined, or not a string, return a default avatar.
+    return {
+      sx: {
+        bgcolor: "gray", // Set the default background color for the avatar
+      },
+      children: "U", // You can set any default value here, like "U" for "Unknown."
+    };
+  }
+
+  const initials = name
+      .split(" ")
+      .map((part) => part[0])
+      .join("");
+
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: initials,
+  };
+}
 
 export default function Header() {
+  const user = useSelector((state) => state.user);
   const [value, setValue] = useState(0);
   const [anchorElUser, setAnchorElUser] = useState(null);
   const navigate = useNavigate();
@@ -84,11 +129,16 @@ export default function Header() {
           </Grid>
           <Grid item xs={2} />
           <Grid item sx={{ placeContent: "left" }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu}>
-                <Avatar sx={{ bgcolor: "#1693a5" }}>RS</Avatar>
-              </IconButton>
-            </Tooltip>
+            {user ? ( // Check if user is not null or undefined
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu}>
+                    <Avatar {...stringAvatar(user.name)}>
+                    </Avatar>
+                  </IconButton>
+                </Tooltip>
+            ) : (
+                <Avatar /> // Replace with your custom fallback avatar component
+            )}
             <Menu
               sx={{ mt: "45px" }}
               id="menu-appbar"
