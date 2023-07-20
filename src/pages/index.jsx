5fbf085040abe654634bcafb137ff5e25786ef98
robyspr <robysprenger@gmail.com>
2023-07-20 00:42:27 -0300

    Manejo de la llamada al backend para loggear al usuario

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,25 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { addUser} from "../redux/userSlice.js";
-
+import { useSelector } from "react-redux";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import RegisterPage from "./RegisterPage/RegisterPage.jsx";
+
 const Page = (props) => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        //fetchData();
-        dispatch(addUser({name: "Roby Sprenger", username: "robyspr", email: "robyspr@email.com"}))
-    }, []);
+  const user = useSelector((state) => state.user);
 
   return (
     <>
-      <Header />
-      <div>{props.children}</div>
-      <Footer />
+      {user.token && user.token.trim().length > 0 ? (
+        <>
+          <Header />
+          <div>{props.children}</div>
+          <Footer />
+        </>
+      ) : (
+          (user.email && user.email.trim().length > 0) ? (
+              <RegisterPage/>
+          ) : (<LoginPage/>)
+      )}
     </>
   );
 };
