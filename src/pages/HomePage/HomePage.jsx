022d0397fef2a5c114dc596682fd6561390d1e0c
robyspr <robysprenger@gmail.com>
2023-07-24 16:57:39 -0300

    Agrego basic Slider para el home

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,8 @@
-import {Grid} from "@mui/material";
-import UserCard from "../../components/HomeCards/UserCard/UserCard.jsx"
-import "../InvestmentPage/InvestmentPage.css";
-
-export default function Home() {
-    return (
-        <Grid container direction="row" spacing={2} className="grid">
-            <Grid item xs={2} />
-            <Grid item xs={3}>
-                <UserCard />
-            </Grid>
-            <Grid item xs={3}>
-                <UserCard />
-            </Grid>
-        </Grid>
+import Slider from "../../components/Home/HomeSlider/Slider.jsx";
+const HomePage = () => {
+    return(
+        <Slider/>
     )
 }
+
+export default HomePage
