951f5a70e6ead469fc9f8079769fa99402bfeede
robyspr <robysprenger@gmail.com>
2023-07-27 15:22:44 -0300

    chau index.css y 80avo fix del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,32 +12,33 @@ export default function Footer() {
       sx={{
         backgroundColor: "#1693a5",
         color: "white",
+        position: "fixed",
+        bottom:0,
+        left:0,
         textAlign: "center",
-        position: "absolute",
-        bottom: 0,
-        left: 0,
         width: "100%",
+        height: "auto",
       }}
     >
       <Container maxWidth="lg">
-        <Grid container spacing={5} sx={{ paddingTop: 2 }}>
-          <Grid item xs={12} sm={4}>
-            <Typography  color="#c7ede8" gutterBottom>
+        <Grid container spacing={5} >
+          <Grid item xs={12} sm={4} >
+            <Typography  color="#c7ede8" gutterBottom sx = {{fontSize: "1.1rem"}}>
               Información
             </Typography>
-            <Typography  sx={{ color: "#a0ded6" , fontSize: "0.8rem"}}>
+            <Typography  sx={{ color: "#a0ded6" , fontSize: "0.9rem"}}>
               Somos ViKatCode y empezamos con este proyecto en julio de 2023
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={4}>
-            <Typography  color="#c7ede8" gutterBottom>
+          <Grid item xs={12} sm={4} sx={{padding:2}}>
+            <Typography  color="#c7ede8" gutterBottom sx = {{fontSize: "1.1rem"}}>
               Contacto
             </Typography>
-            <Typography  sx={{ color: "#a0ded6" , fontSize: "0.8rem"}}>
+            <Typography  sx={{ color: "#a0ded6" , fontSize: "0.9rem"}}>
              vikatcode@bbva.com.ar
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={4} >
+          <Grid item xs={12} sm={4} sx={{padding:2}} >
             <Typography color="#c7ede8" gutterBottom sx = {{fontSize: "1.1rem"}}>
               Redes Sociales
             </Typography>
