9404204b0c28ee27d9fc7daa510d2383936d11a2
robyspr <robysprenger@gmail.com>
2023-07-27 15:28:36 -0300

    Fix de los css

--- src/components/Home/TransactionList/TransactionList.jsx
+++ src/components/Home/TransactionList/TransactionList.jsx
@@ -1,33 +1,10 @@
-import React, { useEffect, useState } from "react";
 import {
-  Card,
-  CardContent,
-  CardHeader,
   Grid,
   List,
-  ListItem,
-  ListItemText,
-  Typography,
 } from "@mui/material";
 import TransactionCard from "../TransactionList/TransactionCard.jsx";
-import TransactionsApi from "../../../api/transactionsApi";
-import { useSelector } from "react-redux";
 
 export default function TransactionList() {
-  const [transactions, setTransactions] = useState([]);
-  const user = useSelector((state) => state.user);
-
-  // useEffect(() => {
-  //   TransactionsApi.getTransactionsByUserId(user.id)
-  //     .then((lista) => {
-  //       console.log(lista);
-  //       setTransactions(lista);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, [user]);
-
   return (
     <Grid
       container
